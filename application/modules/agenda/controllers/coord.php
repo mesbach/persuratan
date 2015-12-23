5cd9c1900ee36f1a5234444dd0149a2aa084594b
mesbach <misbachul.h@gmail.com>
2015-12-23 22:13:49 +0700

    Ini

--- application/modules/agenda/controllers/coord.php
+++ application/modules/agenda/controllers/coord.php
@@ -17,7 +17,7 @@ class Coord extends Koordinator_Controller {
         $this->display('calendarAgenda');
     }
     
-    public function calendarAgenda($id) {
+    public function calendarAgenda($id=null) {
         $this->title="Agenda Kegiatan";
         $this->script_header_spesific = 'lay-scripts/header_calendaragenda';
         $this->script_footer_spesific = 'lay-scripts/footer_calendaragenda';
@@ -45,6 +45,9 @@ class Coord extends Koordinator_Controller {
         $id = $this->saveAgenda();
         $this->createPendamping($id);
         $this->createSat($id);
+        
+        $this->saverundown($id);
+        //redirect('agenda/coord/calendarAgenda');
     }
     public function createPendamping($id){
         $count = $this->input->post('countpendamping');
@@ -67,14 +70,30 @@ class Coord extends Koordinator_Controller {
     public function saveAgenda(){
         $id = $this->session->userdata['logged_in']["id"];
         $data['judul'] = $this->input->post('judul');
-        $data['awal'] = $this->calendar($this->input->post('awal'));
-        $data['akhir'] = $this->calendar($this->input->post('akhir'));   
+        $data['awal'] = date("Y-m-d H:i:00", strtotime($this->input->post('awal')));
+        $data['akhir'] = date("Y-m-d H:i:00", strtotime($this->input->post('akhir')));
         $data['isi'] = $this->input->post('isi');
         $data['hasil'] = $this->input->post('hasil');
         $data['tempat'] = $this->input->post('tempat');
         $data['admin'] = $id;
+        if($this->input->post('mendesak')=='mendesak') $data['ispublic']=1;
         $this->db->insert('agenda',$data);
         return $this->db->insert_id();
+    }
+    public function saverundown($id){
+
+        $count = $this->input->post('tnamard1');
+        echo $count;
+        for($i=1;$i<=$count;$i++){
+            $data['nama'] = $this->input->post('tnamard'.$i);
+            $data['waktu'] = date("Y-m-d H:i:00", strtotime($this->input->post('tjamrd'.$i)));
+            $data['tempat'] = $this->input->post('tempatrd'.$i);
+            $data['pic'] = $this->input->post('picrd'.$i);
+            $data['keterangan'] = $this->input->post('ketrd'.$i);
+            $data['agenda'] = $id;
+            print_r($data); 
+            $this->db->insert('rundown',$data);
+        }
     }
     private function calendar($tanggal)
     {
